279ac383d6b7b1395472ec4fe11308a8584cea59
N-Manjunath <manjunathmanju200727@gmail.com>
2025-07-14 17:12:43 +0530

    done

--- middleware.js
+++ middleware.js
@@ -247,70 +247,70 @@
 
 
 
-const Listing = require("./models/model.js");
-const Review = require("./models/review.js");
-const { listingSchema, reviewSchema } = require("./serverschema.js");
-const ExpressError = require("./utils/ExpressError.js");
+// const Listing = require("./models/model.js");
+// const Review = require("./models/review.js");
+// const { listingSchema, reviewSchema } = require("./serverschema.js");
+// const ExpressError = require("./utils/ExpressError.js");
 
-module.exports.isLoggedin = (req, res, next) => {
-  if (!req.isAuthenticated()) {
-    req.session.redirectUrl = req.originalUrl;
-    req.flash("error", "You must be logged in!");
-    return res.redirect("/login");
-  }
-  next();
-};
+// module.exports.isLoggedin = (req, res, next) => {
+//   if (!req.isAuthenticated()) {
+//     req.session.redirectUrl = req.originalUrl;
+//     req.flash("error", "You must be logged in!");
+//     return res.redirect("/login");
+//   }
+//   next();
+// };
 
-module.exports.saveUrl = (req, res, next) => {
-  if (req.session.redirectUrl) {
-    res.locals.redirectUrl = req.session.redirectUrl;
-  }
-  next();
-};
+// module.exports.saveUrl = (req, res, next) => {
+//   if (req.session.redirectUrl) {
+//     res.locals.redirectUrl = req.session.redirectUrl;
+//   }
+//   next();
+// };
 
-module.exports.isOwner = async (req, res, next) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id).populate("owner");
-  if (!res.locals.CUser._id.equals(listing.owner.id)) {
-    req.flash("error", "Permission required");
-    return res.redirect(`/listings/${id}`);
-  }
-  next();
-};
+// module.exports.isOwner = async (req, res, next) => {
+//   let { id } = req.params;
+//   const listing = await Listing.findById(id).populate("owner");
+//   if (!res.locals.CUser._id.equals(listing.owner.id)) {
+//     req.flash("error", "Permission required");
+//     return res.redirect(`/listings/${id}`);
+//   }
+//   next();
+// };
 
-module.exports.isAuthor = async (req, res, next) => {
-  let { id, reviewid } = req.params;
-  const review = await Review.findById(reviewid);
-  if (!review) {
-    req.flash("error", "Review not found");
-    return res.redirect(`/listings/${id}`);
-  }
-  if (!review.author || !res.locals.CUser._id.equals(review.author)) {
-    req.flash("error", "You are not the author of this review.");
-    return res.redirect(`/listings/${id}`);
-  }
-  next();
-};
+// module.exports.isAuthor = async (req, res, next) => {
+//   let { id, reviewid } = req.params;
+//   const review = await Review.findById(reviewid);
+//   if (!review) {
+//     req.flash("error", "Review not found");
+//     return res.redirect(`/listings/${id}`);
+//   }
+//   if (!review.author || !res.locals.CUser._id.equals(review.author)) {
+//     req.flash("error", "You are not the author of this review.");
+//     return res.redirect(`/listings/${id}`);
+//   }
+//   next();
+// };
 
-module.exports.validatelisting = (req, res, next) => {
-  let { error } = listingSchema.validate(req.body);
-  if (error) {
-    let errmsg = error.details.map(el => el.message).join("\n");
-    throw new ExpressError(errmsg, 900);
-  } else {
-    next();
-  }
-};
+// module.exports.validatelisting = (req, res, next) => {
+//   let { error } = listingSchema.validate(req.body);
+//   if (error) {
+//     let errmsg = error.details.map(el => el.message).join("\n");
+//     throw new ExpressError(errmsg, 900);
+//   } else {
+//     next();
+//   }
+// };
 
-module.exports.validateReviews = (req, res, next) => {
-  let { error } = reviewSchema.validate(req.body);
-  if (error) {
-    let errmsg = error.details.map(el => el.message).join("\n");
-    throw new ExpressError(errmsg, 900);
-  } else {
-    next();
-  }
-};
+// module.exports.validateReviews = (req, res, next) => {
+//   let { error } = reviewSchema.validate(req.body);
+//   if (error) {
+//     let errmsg = error.details.map(el => el.message).join("\n");
+//     throw new ExpressError(errmsg, 900);
+//   } else {
+//     next();
+//   }
+// };
 
 
 
