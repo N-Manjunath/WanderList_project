cef5106e5ca199ca653df4d7f608f3ee8cd2c1e4
N-Manjunath <manjunathmanju200727@gmail.com>
2025-07-14 16:02:14 +0530

    done

--- controllers/user.js
+++ controllers/user.js
@@ -65,6 +65,66 @@
 //     };
 
 
+
+
+
+
+// const User = require("../models/user.js");
+
+// module.exports.renderSignup = (req, res) => {
+//   res.render("./users/signup.ejs");
+// };
+
+// module.exports.Signup = async (req, res, next) => {
+//   try {
+//     const { email, username, password } = req.body;
+//     const newUser = new User({ email, username });
+//     const registeredUser = await User.register(newUser, password);
+//     req.login(registeredUser, err => {
+//       if (err) {
+//         console.log("The error is", err);
+//         return next(err);
+//       }
+//       req.flash("success", "Welcome to Wonder");
+//       res.redirect("/listings");
+//     });
+//   } catch (err) {
+//     console.log("The error is", err.message);
+//     req.flash("error", err.message);
+//     res.redirect("/signup");
+//   }
+// };
+
+// module.exports.renderLogin = (req, res) => {
+//   res.render("./users/login.ejs");
+// };
+
+// module.exports.Login = async (req, res) => {
+//   req.flash("success", "You are logged in!");
+//   const redirectUrl = res.locals.redirectUrl || "/listings";
+//   res.redirect(redirectUrl);
+// };
+
+// module.exports.Logout = async (req, res, next) => {
+//   if (!req.isAuthenticated()) {
+//     req.flash("error", "You aren't logged in");
+//     return res.redirect("/login");
+//   }
+//   try {
+//     await req.logout();
+//     req.flash("success", "You are logged out");
+//     res.redirect("/listings");
+//   } catch (err) {
+//     console.log(err);
+//     return next(err);
+//   }
+// };
+
+
+
+
+
+
 const User = require("../models/user.js");
 
 module.exports.renderSignup = (req, res) => {
@@ -73,19 +133,20 @@ module.exports.renderSignup = (req, res) => {
 
 module.exports.Signup = async (req, res, next) => {
   try {
-    const { email, username, password } = req.body;
-    const newUser = new User({ email, username });
+    let { email, username, password } = req.body;
+    let newUser = new User({ email, username });
     const registeredUser = await User.register(newUser, password);
-    req.login(registeredUser, err => {
+
+    req.login(registeredUser, (err) => {
       if (err) {
-        console.log("The error is", err);
+        console.log("The error is:", err);
         return next(err);
       }
       req.flash("success", "Welcome to Wonder");
       res.redirect("/listings");
     });
   } catch (err) {
-    console.log("The error is", err.message);
+    console.log("The error is:", err.message);
     req.flash("error", err.message);
     res.redirect("/signup");
   }
@@ -97,21 +158,21 @@ module.exports.renderLogin = (req, res) => {
 
 module.exports.Login = async (req, res) => {
   req.flash("success", "You are logged in!");
-  const redirectUrl = res.locals.redirectUrl || "/listings";
+  let redirectUrl = res.locals.redirectUrl || "/listings";
   res.redirect(redirectUrl);
 };
 
-module.exports.Logout = async (req, res, next) => {
+module.exports.Logout = (req, res, next) => {
   if (!req.isAuthenticated()) {
     req.flash("error", "You aren't logged in");
     return res.redirect("/login");
   }
-  try {
-    await req.logout();
+
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
     req.flash("success", "You are logged out");
     res.redirect("/listings");
-  } catch (err) {
-    console.log(err);
-    return next(err);
-  }
+  });
 };
--- middleware.js
+++ middleware.js
@@ -158,6 +158,95 @@
 
 
 
+// const Listing = require("./models/model.js");
+// const Review = require("./models/review.js");
+// const { listingSchema, reviewSchema } = require("./serverschema.js");
+// const ExpressError = require("./utils/ExpressError.js");
+
+// module.exports.isLoggedin = (req, res, next) => {
+//   if (!req.isAuthenticated()) {
+//     req.session.redirectUrl = req.originalUrl;
+//     req.flash("error", "You must be logged in!");
+//     return res.redirect("/login");
+//   }
+//   next();
+// };
+
+// module.exports.saveUrl = (req, res, next) => {
+//   if (req.session.redirectUrl) {
+//     res.locals.redirectUrl = req.session.redirectUrl;
+//   }
+//   next();
+// };
+
+// module.exports.isOwner = async (req, res, next) => {
+//   const { id } = req.params;
+//   const listing = await Listing.findById(id).populate("owner");
+
+//   if (!listing) {
+//     req.flash("error", "Listing not found");
+//     return res.redirect("/listings");
+//   }
+
+//   if (!listing.owner) {
+//     req.flash("error", "Listing has no owner");
+//     return res.redirect(`/listings/${id}`);
+//   }
+
+//   if (!res.locals.CUser || !res.locals.CUser._id.equals(listing.owner.id)) {
+//     req.flash("error", "Permission required");
+//     return res.redirect(`/listings/${id}`);
+//   }
+
+//   next();
+// };
+
+// module.exports.isAuthor = async (req, res, next) => {
+//   const { id, reviewid } = req.params;
+//   const review = await Review.findById(reviewid);
+
+//   if (!review) {
+//     req.flash("error", "Review not found");
+//     return res.redirect(`/listings/${id}`);
+//   }
+
+//   if (!review.author || !res.locals.CUser._id.equals(review.author)) {
+//     req.flash("error", "You are not the author of this review.");
+//     return res.redirect(`/listings/${id}`);
+//   }
+
+//   next();
+// };
+
+// module.exports.validatelisting = (req, res, next) => {
+//   const { error } = listingSchema.validate(req.body);
+//   if (error) {
+//     const errmsg = error.details.map(el => el.message).join("\n");
+//     throw new ExpressError(errmsg, 900);
+//   }
+//   next();
+// };
+
+// module.exports.validateReviews = (req, res, next) => {
+//   const { error } = reviewSchema.validate(req.body);
+//   if (error) {
+//     const errmsg = error.details.map(el => el.message).join("\n");
+//     throw new ExpressError(errmsg, 900);
+//   }
+//   next();
+// };
+
+
+
+
+
+
+
+
+
+
+
+
 const Listing = require("./models/model.js");
 const Review = require("./models/review.js");
 const { listingSchema, reviewSchema } = require("./serverschema.js");
@@ -180,59 +269,47 @@ module.exports.saveUrl = (req, res, next) => {
 };
 
 module.exports.isOwner = async (req, res, next) => {
-  const { id } = req.params;
+  let { id } = req.params;
   const listing = await Listing.findById(id).populate("owner");
-
-  if (!listing) {
-    req.flash("error", "Listing not found");
-    return res.redirect("/listings");
-  }
-
-  if (!listing.owner) {
-    req.flash("error", "Listing has no owner");
-    return res.redirect(`/listings/${id}`);
-  }
-
-  if (!res.locals.CUser || !res.locals.CUser._id.equals(listing.owner.id)) {
+  if (!res.locals.CUser._id.equals(listing.owner.id)) {
     req.flash("error", "Permission required");
     return res.redirect(`/listings/${id}`);
   }
-
   next();
 };
 
 module.exports.isAuthor = async (req, res, next) => {
-  const { id, reviewid } = req.params;
+  let { id, reviewid } = req.params;
   const review = await Review.findById(reviewid);
-
   if (!review) {
     req.flash("error", "Review not found");
     return res.redirect(`/listings/${id}`);
   }
-
   if (!review.author || !res.locals.CUser._id.equals(review.author)) {
     req.flash("error", "You are not the author of this review.");
     return res.redirect(`/listings/${id}`);
   }
-
   next();
 };
 
 module.exports.validatelisting = (req, res, next) => {
-  const { error } = listingSchema.validate(req.body);
+  let { error } = listingSchema.validate(req.body);
   if (error) {
-    const errmsg = error.details.map(el => el.message).join("\n");
+    let errmsg = error.details.map(el => el.message).join("\n");
     throw new ExpressError(errmsg, 900);
+  } else {
+    next();
   }
-  next();
 };
 
 module.exports.validateReviews = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
+  let { error } = reviewSchema.validate(req.body);
   if (error) {
-    const errmsg = error.details.map(el => el.message).join("\n");
+    let errmsg = error.details.map(el => el.message).join("\n");
     throw new ExpressError(errmsg, 900);
+  } else {
+    next();
   }
-  next();
 };
+
 
