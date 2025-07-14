151abfaf9bad79fe077c9804afe22666e94cfbe8
N-Manjunath <manjunathmanju200727@gmail.com>
2025-07-14 15:19:09 +0530

    ntg

--- middleware.js
+++ middleware.js
@@ -87,6 +87,77 @@
 //     }
 //   }
 
+// const Listing = require("./models/model.js");
+// const Review = require("./models/review.js");
+// const { listingSchema, reviewSchema } = require("./serverschema.js");
+// const ExpressError = require("./utils/ExpressError.js");
+
+// module.exports.isLoggedin = (req, res, next) => {
+//   if (!req.isAuthenticated()) {
+//     req.session.redirectUrl = req.originalUrl;
+//     req.flash("error", "You must be logged in!");
+//     return res.redirect("/login");
+//   }
+//   next();
+// };
+
+// module.exports.saveUrl = (req, res, next) => {
+//   // Makes the URL available in res.locals for templates,
+//   // but controllers should always read req.session!
+//   if (req.session.redirectUrl) {
+//     res.locals.redirectUrl = req.session.redirectUrl;
+//   }
+//   next();
+// };
+
+// module.exports.isOwner = async (req, res, next) => {
+//   let { id } = req.params;
+//   const listing = await Listing.findById(id).populate("owner");
+//   if (!res.locals.CUser._id.equals(listing.owner.id)) {
+//     req.flash("error", "Permission required");
+//     return res.redirect(`/listings/${id}`);
+//   }
+//   next();
+// };
+
+// module.exports.isAuthor = async (req, res, next) => {
+//   let { id, reviewid } = req.params;
+//   const review = await Review.findById(reviewid);
+//   if (!review) {
+//     req.flash("error", "Review not found");
+//     return res.redirect(`/listings/${id}`);
+//   }
+//   if (!review.author || !res.locals.CUser._id.equals(review.author)) {
+//     req.flash("error", "You are not the author of this review.");
+//     return res.redirect(`/listings/${id}`);
+//   }
+//   next();
+// };
+
+// module.exports.validatelisting = (req, res, next) => {
+//   let { error } = listingSchema.validate(req.body);
+//   if (error) {
+//     let errmsg = error.details.map((el) => el.message).join("\n");
+//     throw new ExpressError(errmsg, 900);
+//   } else {
+//     next();
+//   }
+// };
+
+// module.exports.validateReviews = (req, res, next) => {
+//   let { error } = reviewSchema.validate(req.body);
+//   if (error) {
+//     let errmsg = error.details.map((el) => el.message).join("\n");
+//     throw new ExpressError(errmsg, 900);
+//   } else {
+//     next();
+//   }
+// };
+
+
+
+
+
 const Listing = require("./models/model.js");
 const Review = require("./models/review.js");
 const { listingSchema, reviewSchema } = require("./serverschema.js");
@@ -102,8 +173,6 @@ module.exports.isLoggedin = (req, res, next) => {
 };
 
 module.exports.saveUrl = (req, res, next) => {
-  // Makes the URL available in res.locals for templates,
-  // but controllers should always read req.session!
   if (req.session.redirectUrl) {
     res.locals.redirectUrl = req.session.redirectUrl;
   }
@@ -111,46 +180,59 @@ module.exports.saveUrl = (req, res, next) => {
 };
 
 module.exports.isOwner = async (req, res, next) => {
-  let { id } = req.params;
+  const { id } = req.params;
   const listing = await Listing.findById(id).populate("owner");
-  if (!res.locals.CUser._id.equals(listing.owner.id)) {
+
+  if (!listing) {
+    req.flash("error", "Listing not found");
+    return res.redirect("/listings");
+  }
+
+  if (!listing.owner) {
+    req.flash("error", "Listing has no owner");
+    return res.redirect(`/listings/${id}`);
+  }
+
+  if (!res.locals.CUser || !res.locals.CUser._id.equals(listing.owner.id)) {
     req.flash("error", "Permission required");
     return res.redirect(`/listings/${id}`);
   }
+
   next();
 };
 
 module.exports.isAuthor = async (req, res, next) => {
-  let { id, reviewid } = req.params;
+  const { id, reviewid } = req.params;
   const review = await Review.findById(reviewid);
+
   if (!review) {
     req.flash("error", "Review not found");
     return res.redirect(`/listings/${id}`);
   }
+
   if (!review.author || !res.locals.CUser._id.equals(review.author)) {
     req.flash("error", "You are not the author of this review.");
     return res.redirect(`/listings/${id}`);
   }
+
   next();
 };
 
 module.exports.validatelisting = (req, res, next) => {
-  let { error } = listingSchema.validate(req.body);
+  const { error } = listingSchema.validate(req.body);
   if (error) {
-    let errmsg = error.details.map((el) => el.message).join("\n");
+    const errmsg = error.details.map(el => el.message).join("\n");
     throw new ExpressError(errmsg, 900);
-  } else {
-    next();
   }
+  next();
 };
 
 module.exports.validateReviews = (req, res, next) => {
-  let { error } = reviewSchema.validate(req.body);
+  const { error } = reviewSchema.validate(req.body);
   if (error) {
-    let errmsg = error.details.map((el) => el.message).join("\n");
+    const errmsg = error.details.map(el => el.message).join("\n");
     throw new ExpressError(errmsg, 900);
-  } else {
-    next();
   }
+  next();
 };
 
